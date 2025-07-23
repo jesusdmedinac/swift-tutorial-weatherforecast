e90ecaa3ea9adf0b982abf793667b8c50572a395
Jesus Daniel Medina Cruz <jesus.daniel.medina.cruz@gmail.com>
2025-07-23 17:12:43 -0400

    Tutorial ended

--- WeatherForecast/ContentView.swift
+++ WeatherForecast/ContentView.swift
@@ -9,16 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+      HStack {
+        DayForecast(day: "Mon", isRainy: false, high: 70, low: 50)
+        
+        DayForecast(day: "Tue", isRainy: true, high: 60, low: 40)
+      }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct DayForecast: View {
+  let day: String
+  let isRainy: Bool
+  let high: Int
+  let low: Int
+  
+  var iconName: String {
+    if isRainy {
+      return "cloud.rain.fill"
+    } else {
+      return "sun.max.fill"
+    }
+  }
+  
+  var iconColor: Color {
+    if isRainy {
+      return .blue
+    } else {
+      return .yellow
+    }
+  }
+  
+  var body: some View {
+    VStack {
+      Text(day)
+      Image(systemName: iconName)
+        .foregroundColor(iconColor)
+        .font(Font.largeTitle)
+        .padding(5)
+      Text("High: \(high)")
+        .fontWeight(Font.Weight.semibold)
+      Text("Low: \(low)")
+        .fontWeight(Font.Weight.medium)
+        .foregroundStyle(Color.secondary)
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  DayForecast(day: "Mon", isRainy: false, high: 30, low: 20)
 }
